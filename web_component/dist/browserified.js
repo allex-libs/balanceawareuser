46a9e99ec3c9182a0b198701c18dd1e2f7da35aa
Andrija Petrovic <andrija.hers@gmail.com>
2018-04-24 12:39:48 +0000

    + the web_component

--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -5,22 +5,65 @@ ALLEX.execSuite.libRegistry.register('allex_balanceawareuserlib',require('./brow
 function createLib (execlib) {
   'use strict';
 
+  var templates = require('./templatecreator')(execlib);
+
   return {
     methoddescriptors: {
-      serviceuser: require('./methoddescriptors/serviceuser')
+      serviceuser: require('./methoddescriptors/serviceuser')(templates)
     }
   };
 }
 
 module.exports = createLib;
 
-},{"./methoddescriptors/serviceuser":3}],3:[function(require,module,exports){
-module.exports = {
-  updateBalance: [{
-    title: 'New Balance',
-    type: 'integer'
-  }]
-};
+},{"./methoddescriptors/serviceuser":3,"./templatecreator":4}],3:[function(require,module,exports){
+function createMethodDescriptors (templates) {
+  'use strict';
+  return {
+    updateBalance: templates.methodDescriptorUpdateBalance
+  };
+}
 
+
+module.exports = createMethodDescriptors;
+
+
+},{}],4:[function(require,module,exports){
+function createTemplate (execlib) {
+  'use strict';
+
+
+  function serviceBalanceAwareFunc (statename) {
+    return function (balanceinteger) {
+      this.state.set(statename, this.msm ? this.msm.fromMoney(balanceinteger) : balanceinteger);
+    };
+  }
+
+  function serviceUserUpdateBalanceFunc (servicemethodname) {
+    return function (newbalance, defer) {
+      this.__service[servicemethodname](newbalance);
+      defer.resolve(newbalance);
+    }
+  }
+
+  function ctorBalanceSetterFunc (service, hotelfetchermethodname, servicesettermethodname) {
+    service.__hotel[hotelfetchermethodname](service.name).then(service[servicesettermethodname].bind(service));
+    service = null;
+    hotelfetchermethodname = null;
+    servicesettermethodname = null;
+  }
+
+  return {
+    serviceBalanceAwareFunc: serviceBalanceAwareFunc,
+    ctorBalanceSetterFunc: ctorBalanceSetterFunc,
+    serviceUserUpdateBalanceFunc: serviceUserUpdateBalanceFunc,
+    methodDescriptorUpdateBalance: [{
+      title: 'New Balance',
+      type: 'integer'
+    }]
+  };
+}
+
+module.exports = createTemplate;
 
 },{}]},{},[1]);
